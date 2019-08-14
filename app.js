49f2840f0b7597dda7bddb833a3df2b65ecfddb9
Josiah Aubin <josiahaubin@gmail.com>
2019-08-14 14:39:54 -0600

    Randomized computer choice

--- app.js
+++ app.js
@@ -3,39 +3,59 @@
 //compared to the computers choice
 //It then should tell the user who won.
 function play(playerChoice) {
-  let compChoice = 'scissors';
+
+  let compSelect = randComputerChoice();
 
   //If player picks rock
   if (playerChoice == 'rock') {
 
-    if (compChoice == 'rock') {
-      alert(`Computer picked ${compChoice} so you tied`);
-    } else if (compChoice == 'paper') {
-      alert(`Computer picked ${compChoice} so you lose`);
+    if (compSelect == 'rock') {
+      alert(`Computer picked ${compSelect} so you tied`);
+    } else if (compSelect == 'paper') {
+      alert(`Computer picked ${compSelect} so you lose`);
     } else {
-      alert(`Computer picked ${compChoice} so you won`)
+      alert(`Computer picked ${compSelect} so you won`)
     }
 
     //If player picks paper
   } else if (playerChoice == 'paper') {
 
-    if (compChoice == 'rock') {
-      alert(`Computer picked ${compChoice} so you won`);
-    } else if (compChoice == 'paper') {
-      alert(`Computer picked ${compChoice} so you tied`);
+    if (compSelect == 'rock') {
+      alert(`Computer picked ${compSelect} so you won`);
+    } else if (compSelect == 'paper') {
+      alert(`Computer picked ${compSelect} so you tied`);
     } else {
-      alert(`Computer picked ${compChoice} so you lose`);
+      alert(`Computer picked ${compSelect} so you lose`);
     }
 
     //If player picks scissors
   } else {
 
-    if (compChoice == 'paper') {
-      alert(`Computer picked ${compChoice} so you won`);
-    } else if (compChoice == 'rock') {
-      alert(`Computer picked ${compChoice} so you lose`);
+    if (compSelect == 'paper') {
+      alert(`Computer picked ${compSelect} so you won`);
+    } else if (compSelect == 'rock') {
+      alert(`Computer picked ${compSelect} so you lose`);
     } else {
-      alert(`Computer picked ${compChoice} so you tied`);
+      alert(`Computer picked ${compSelect} so you tied`);
     }
   }
+}
+//This function will randomize the computers choice
+//and return a value to be used in play()
+function randComputerChoice() {
+
+  let randNum = Math.floor((Math.random() * 3));
+  let compSelect;
+
+  if (randNum == 0) {
+    compSelect = 'rock';
+    return compSelect;
+  } else if (randNum == 1) {
+    compSelect = 'paper';
+    return compSelect;
+  } else {
+    compSelect = 'scissors'
+    return compSelect;
+  }
+
 }
