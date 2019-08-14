109fbdc7f4dceeefd0e2f81d3a19da3f90542542
Josiah Aubin <josiahaubin@gmail.com>
2019-08-14 15:28:04 -0600

    Results section functionality complete

--- app.js
+++ app.js
@@ -4,41 +4,43 @@
 //It then should tell the user who won.
 function play(playerChoice) {
 
+  let result;
   let compSelect = randComputerChoice();
 
   //If player picks rock
   if (playerChoice == 'rock') {
 
     if (compSelect == 'rock') {
-      alert(`Computer picked ${compSelect} so you tied`);
+      result = `Computer picked ${compSelect} so you tied`;
     } else if (compSelect == 'paper') {
-      alert(`Computer picked ${compSelect} so you lose`);
+      result = `Computer picked ${compSelect} so you lose`;
     } else {
-      alert(`Computer picked ${compSelect} so you won`)
+      result = `Computer picked ${compSelect} so you won`;
     }
 
     //If player picks paper
   } else if (playerChoice == 'paper') {
 
     if (compSelect == 'rock') {
-      alert(`Computer picked ${compSelect} so you won`);
+      result = `Computer picked ${compSelect} so you won`;
     } else if (compSelect == 'paper') {
-      alert(`Computer picked ${compSelect} so you tied`);
+      result = `Computer picked ${compSelect} so you tied`;
     } else {
-      alert(`Computer picked ${compSelect} so you lose`);
+      result = `Computer picked ${compSelect} so you lose`;
     }
 
     //If player picks scissors
   } else {
 
     if (compSelect == 'paper') {
-      alert(`Computer picked ${compSelect} so you won`);
+      result = `Computer picked ${compSelect} so you won`;
     } else if (compSelect == 'rock') {
-      alert(`Computer picked ${compSelect} so you lose`);
+      result = `Computer picked ${compSelect} so you lose`;
     } else {
-      alert(`Computer picked ${compSelect} so you tied`);
+      result = `Computer picked ${compSelect} so you tied`;
     }
   }
+  draw(result);
 }
 //This function will randomize the computers choice
 //and return a value to be used in play()
@@ -59,3 +61,28 @@ function randComputerChoice() {
   }
 
 }
+let winCount = 0;
+let tieCount = 0;
+let lossCount = 0;
+
+function draw(result) {
+  let resultElement = document.querySelector("#resultHeading");
+  let countElement = document.querySelector("#runningCountHeading");
+
+  resultElement.textContent = result;
+
+
+  let win = result.indexOf("won");
+  let tie = result.indexOf('tied');
+  let loss = result.indexOf('lose');
+
+  if (win != -1) {
+    winCount++;
+  } else if (tie != -1) {
+    tieCount++;
+  } else if (loss != -1) {
+    lossCount++;
+  }
+  countElement.textContent = "Wins: " + winCount + " Ties: " + tieCount + " Losses: " + lossCount;
+}
+
